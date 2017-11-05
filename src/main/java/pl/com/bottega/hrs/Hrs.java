d6c8db8a42a285e2502373d125f31ef39aa1ccef
Maciej Zubala <maciej.zubala@gmail.com>
2017-11-05 11:14:42 +0100

    spring

---
+++ src/main/java/pl/com/bottega/hrs/Hrs.java
@@ -0,0 +1,13 @@
+package pl.com.bottega.hrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hrs {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hrs.class, args);
+    }
+
+}
